00d9b023ef7ee7eeb72427450ca21ed0c84abdd3
jashwanth9845 <74336314+jashwanth9845@users.noreply.github.com>
2020-11-19 08:16:59 +0530

    Update sqlconnecton.php

--- company_web_project_7th_sem/sqlconnecton.php
+++ company_web_project_7th_sem/sqlconnecton.php
@@ -4,17 +4,6 @@
 	<title>mysql wtth php</title>
 <body>
 <?php 
-$server_name='localhost:3307';
-$user_name='root';
-$password="";
-$database="company";
-
-$connection=new mysqli($server_name,$user_name,$password,$database);
-if($connection->connect_error)
-{
-	die("unableto connect".$connection->connect_error);
-
-}
 echo "database connection successfully";
  ?>
 
